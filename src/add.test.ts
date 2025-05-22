1a01def6533613134d0b8bfe23433f5640554c94
f-ffff-f <ghr95223@gmail.com>
2025-05-22 14:04:25 +0900

    chore: initial project setup with TypeScript and Vitest

---
+++ src/add.test.ts
@@ -0,0 +1,13 @@
+import { add } from '@/add'
+
+describe('add', () => {
+  it('should add two numbers and return the result', () => {
+    const num1 = 1
+    const num2 = 2
+
+    const expected = 3
+    const actual = add(num1, num2)
+
+    expect(actual).toBe(expected)
+  })
+})
