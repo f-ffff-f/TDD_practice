efe8bd66077835e1b00c8ade348e9acaa36bcfb3
f-ffff-f <ghr95223@gmail.com>
2025-05-24 14:07:45 +0900

    chore: split into basic-practice & todo-api packages

---
+++ src/basic-practice/index.ts
@@ -0,0 +1,5 @@
+// Export the modules from the add and user directories
+export * from './add/add.js';
+export * from './user/UserService.js';
+export * from './user/EmailClient.js';
+export * from './user/UserWelcomeEmailSenderApp.js';
---
+++ src/todo-api/index.ts
@@ -0,0 +1,7 @@
+// Export all modules from todo-api
+export * from './in-memory.db.interface.js';
+export * from './in-memory.db.js';
+export * from './server.factory.js';
+export * from './server.js';
+export * from './todo.store.interface.js';
+export * from './todo.store.js';
